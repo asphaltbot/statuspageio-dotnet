07413c7b6d2f808a8a43a6a35e8349cd71e62e7c
Connor Wright <connor@connorwright.uk>
2024-10-21 18:09:54 +0100

    Add "Maintenance" to incident impact, fixing GetScheduledIncidents method

--- StatusPageIo/StatusPageIo.Api/Models/Incidents/IncidentImpact.cs
+++ StatusPageIo/StatusPageIo.Api/Models/Incidents/IncidentImpact.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StatusPageIo.Api.Models.Incidents
+﻿namespace StatusPageIo.Api.Models.Incidents
 {
     public enum IncidentImpact
     {
         None,
         Minor,
         Major,
-        Critical
+        Critical,
+        Maintenance
     }
 }
